2c80cc8c2747f9a2a5279e6c06c2ecdb95bc27a2
nader <abolfazli@coin-plus.org>
2021-12-13 17:25:30 +0330

    fix: comment post register signal handling to created mail message

--- drf_user/signals/handlers.py
+++ drf_user/signals/handlers.py
@@ -22,18 +22,19 @@ def post_register(sender, instance: get_user_model(), created, **kwargs):
     from drfaddons.utils import send_message
 
     if created:
-        if user_settings["REGISTRATION"]["SEND_MAIL"]:
-            send_message(
-                message=user_settings["REGISTRATION"]["TEXT_MAIL_BODY"],
-                subject=user_settings["REGISTRATION"]["MAIL_SUBJECT"],
-                recip=[instance.email],
-                recip_email=[instance.email],
-                html_message=user_settings["REGISTRATION"]["HTML_MAIL_BODY"],
-            )
-        if user_settings["REGISTRATION"]["SEND_MESSAGE"]:
-            send_message(
-                message=user_settings["REGISTRATION"]["SMS_BODY"],
-                subject=user_settings["REGISTRATION"]["MAIL_SUBJECT"],
-                recip=[instance.mobile],
-                recip_email=[instance.mobile],
-            )
+        # if user_settings["REGISTRATION"]["SEND_MAIL"]:
+        #     send_message(
+        #         message=user_settings["REGISTRATION"]["TEXT_MAIL_BODY"],
+        #         subject=user_settings["REGISTRATION"]["MAIL_SUBJECT"],
+        #         recip=[instance.email],
+        #         recip_email=[instance.email],
+        #         html_message=user_settings["REGISTRATION"]["HTML_MAIL_BODY"],
+        #     )
+        # if user_settings["REGISTRATION"]["SEND_MESSAGE"]:
+        #     send_message(
+        #         message=user_settings["REGISTRATION"]["SMS_BODY"],
+        #         subject=user_settings["REGISTRATION"]["MAIL_SUBJECT"],
+        #         recip=[instance.mobile],
+        #         recip_email=[instance.mobile],
+        #     )
+        pass
